31010a9302046f305ec68e4ab1209db02b7d0156
XpcMan <99084826+XpcMan@users.noreply.github.com>
2022-03-04 11:53:55 +0530

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+const user = {
+    phone_number:""
+}
+
+router.get('/',(req,res)=>{
+    res.send(user.phone_number);
+})
+
+router.post('/',(req,res)=>{
+    user.phone_number = req.body.data;
+
+    res.send('New Phone Number :<h1> '+user.phone_number+'</h1>');
+})
+
+module.exports =  router;
